87ada66e96215b673b3a9fbf0b1f9eba3dfc74ff
joopitz <jonas.opitz@live.de>
2023-07-31 14:29:39 +0200

    minor: optionally simplify imports

---
+++ nlprep/__init__.py
@@ -0,0 +1,6 @@
+import nlprep.specs.collections as collections
+import nlprep.specs.pipelines as pipelines
+import nlprep.specs.filters as filters
+
+from nlprep.types import *
+from nlprep.core import *
---
+++ nlprep/spacy/__init__.py
@@ -0,0 +1,1 @@
+from nlprep.spacy.props import *
