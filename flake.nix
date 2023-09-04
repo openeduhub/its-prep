9f542681d538b39933d2cdbc3cb8fdc7b1403d54
joopitz <jonas.opitz@live.de>
2023-09-04 16:35:19 +0200

    feat(nix): parameterize package by python
    This makes it possible to use the provided python library without having to
    make sure that the imported flake follows the own flake's inputs.

--- flake.nix
+++ flake.nix
@@ -10,43 +10,39 @@
   outputs = { self, nixpkgs, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = import nixpkgs {inherit system;};
+        pkgs = import nixpkgs { inherit system; };
         python = pkgs.python310;
         nix-filter = self.inputs.nix-filter.lib;
 
         # build the spaCy language processing pipeline as a python package
-        de_dep_news_trf = with pkgs.python3Packages;
-          buildPythonPackage rec {
-            pname = "de_dep_news_trf";
-            version = "3.5.0";
-            src = pkgs.fetchzip {
-              url = "https://github.com/explosion/spacy-models/releases/download/${pname}-${version}/${pname}-${version}.tar.gz";
-              hash = "sha256-MleftGMj+VK8hAE+EOg0VhtFUg/oOH3grPbRzklyiVE=";
-            };
-            doCheck = false;
-            propagatedBuildInputs = [
-              spacy
-              spacy-transformers
-            ];
+        de_dep_news_trf = py-pkgs: py-pkgs.buildPythonPackage rec {
+          pname = "de_dep_news_trf";
+          version = "3.5.0";
+          src = pkgs.fetchzip {
+            url = "https://github.com/explosion/spacy-models/releases/download/${pname}-${version}/${pname}-${version}.tar.gz";
+            hash = "sha256-MleftGMj+VK8hAE+EOg0VhtFUg/oOH3grPbRzklyiVE=";
           };
+          doCheck = false;
+          propagatedBuildInputs = with py-pkgs; [ spacy spacy-transformers ];
+        };
 
         ### declare the python packages used for building, docs & development
-        python-packages-build = python-packages:
-          with python-packages; [
-            de_dep_news_trf
+        python-packages-build = py-pkgs:
+          with py-pkgs; [
+            (de_dep_news_trf py-pkgs)
             numpy
             spacy
           ];
-        
-        python-packages-docs = python-packages:
-          with python-packages; [
+
+        python-packages-docs = py-pkgs:
+          with py-pkgs; [
             sphinx
             sphinx-rtd-theme
             sphinx-autodoc-typehints
           ];
 
-        python-packages-devel = python-packages:
-          with python-packages; [
+        python-packages-devel = py-pkgs:
+          with py-pkgs; [
             # coding utilities
             black
             flake8
@@ -59,11 +55,11 @@
             pytest-cov
             hypothesis
           ]
-          ++ (python-packages-build python-packages)
-          ++ (python-packages-docs  python-packages);
+          ++ (python-packages-build py-pkgs)
+          ++ (python-packages-docs py-pkgs);
 
         ### declare how the python package shall be built
-        nlprep = with python.pkgs; buildPythonPackage rec {
+        nlprep-lib = py-pkgs: py-pkgs.buildPythonPackage rec {
           pname = "nlprep";
           version = "0.1.2";
           # only include the package-related files
@@ -77,32 +73,42 @@
             ];
             exclude = [ (nix-filter.matchExt "pyc") ];
           };
-          propagatedBuildInputs = (python-packages-build python.pkgs);
+          propagatedBuildInputs = (python-packages-build py-pkgs);
           # use pytestCheckHook to run pytest after building
-          nativeCheckInputs = [ pytestCheckHook hypothesis ];
+          nativeCheckInputs = with py-pkgs; [
+            pytestCheckHook
+            hypothesis
+          ];
         };
+        nlprep = nlprep-lib python.pkgs;
 
         ### declare build system for the documentation
-        docs = pkgs.runCommand "docs" {
-          buildInputs = [
-            (python-packages-docs python.pkgs)
-            (nlprep.override {doCheck = false;})
-          ];
-        } (pkgs.writeShellScript "docs.sh" ''
+        docs = pkgs.runCommand "docs"
+          {
+            buildInputs = [
+              (python-packages-docs python.pkgs)
+              (nlprep.override { doCheck = false; })
+            ];
+          }
+          (pkgs.writeShellScript "docs.sh" ''
             sphinx-build -b html ${./docs} $out
-          ''); 
-
-      in {
-        packages = rec {
+          '');
+      in
+      {
+        packages = {
           inherit nlprep docs;
           default = nlprep;
         };
+        lib = {
+          nlprep = nlprep-lib;
+        };
         devShells.default = pkgs.mkShell {
           buildInputs = [
-            (python-packages-devel python.pkgs)
+            (python.withPackages python-packages-devel)
             # python language server
             pkgs.nodePackages.pyright
           ];
         };
       }
-    );}
+    );
+}
