e28b9a06e485b2e6e37bbc325751ee4675f00a8a
joopitz <jonas.opitz@live.de>
2023-10-11 08:35:17 +0200

    minor: make spacy tests more readable

--- test/test_spacy.py
+++ test/test_spacy.py
@@ -1,13 +1,16 @@
 from collections.abc import Callable, Collection
-from nlprep.types import Document, Property_Function, Tokens
-from test.strategies import documents, texts
+from nlprep.types import Document, Property_Function, Split_Function, Tokens
+import test.strategies as nlp_st
 
 import nlprep.spacy.props as nlp
 from hypothesis import given
 from hypothesis import strategies as st
 
 
-@given(texts, st.sampled_from([nlp.tokenize_as_lemmas, nlp.tokenize_as_words]))
+@given(
+    nlp_st.texts,
+    st.sampled_from([nlp.tokenize_as_lemmas, nlp.tokenize_as_words]),
+)
 def test_tokenizers_do_something(text: str, tokenizer: Callable[[str], Tokens]):
     result = tokenizer(text)
 
@@ -15,16 +18,19 @@ def test_tokenizers_do_something(text: str, tokenizer: Callable[[str], Tokens]):
         assert result
 
 
-@given(documents, st.sampled_from([nlp.get_upos, nlp.lemmatize, nlp.is_stop]))
+@given(
+    nlp_st.documents,
+    st.sampled_from([nlp.get_upos, nlp.lemmatize, nlp.is_stop]),
+)
 def test_properties_have_correct_len(doc: Document, fun: Property_Function):
     result = fun(doc)
     assert len(result) == len(doc.original_tokens)
 
 
-@given(texts, st.sampled_from([nlp.into_sentences, nlp.into_sentences_lemmatized]))
-def test_splitters_have_correct_len(
-    text: str, tokenizer: Callable[[Document], Collection[Collection]]
-):
-    doc = Document.fromtokens(nlp.tokenize_as_words(text))
-    split_result = tokenizer(doc)
+@given(
+    nlp_st.documents,
+    st.sampled_from([nlp.into_sentences, nlp.into_sentences_lemmatized]),
+)
+def test_splitters_have_correct_len(doc: Document, splitter: Split_Function):
+    split_result = splitter(doc)
     assert sum(len(result) for result in split_result) == len(doc.original_tokens)
