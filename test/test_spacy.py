754f17aeedcc1b8ff634c06a3ef9fbceed516039
joopitz <jonas.opitz@live.de>
2023-11-06 12:32:03 +0100

    chore: add tests for new caching functionality

--- test/test_spacy.py
+++ test/test_spacy.py
@@ -1,6 +1,7 @@
 from collections.abc import Callable, Collection
 from nlprep.types import Document, Property_Function, Split_Function, Tokens
 import test.strategies as nlp_st
+from pathlib import Path
 
 import nlprep.spacy.props as nlp
 from hypothesis import given
@@ -34,3 +35,53 @@ def test_properties_have_correct_len(doc: Document, fun: Property_Function):
 def test_splitters_have_correct_len(doc: Document, splitter: Split_Function):
     split_result = splitter(doc)
     assert sum(len(result) for result in split_result) == len(doc.original_tokens)
+
+
+@given(nlp_st.texts)
+def test_text_cache(text: str):
+    doc = nlp.tokenize_as_words(text)
+
+    assert text in nlp.text_to_doc_cache
+    for token_doc, token_cache in zip(doc, nlp.text_to_doc_cache[text]):
+        assert str(token_doc) == str(token_cache)
+
+
+@given(nlp_st.tokens)
+def test_tokens_cache(tokens: Tokens):
+    doc = nlp._spacy_doc_from_tokens(tokens)
+
+    assert tokens in nlp.tokens_to_doc_cache
+    for token_doc, token_cache in zip(doc, nlp.tokens_to_doc_cache[tokens]):
+        assert str(token_doc) == str(token_cache)
+
+
+@given(nlp_st.texts)
+def test_text_cache_storage(text: str):
+    doc = nlp.tokenize_as_words(text)
+
+    # store the cache
+    nlp.save_caches(Path("/tmp"), file_prefix="pytest")
+    # delete the text from the cache
+    del nlp.text_to_doc_cache[text]
+    # load the cache
+    nlp.load_caches(Path("/tmp"), file_prefix="pytest")
+
+    assert text in nlp.text_to_doc_cache
+    for token_doc, token_cache in zip(doc, nlp.text_to_doc_cache[text]):
+        assert str(token_doc) == str(token_cache)
+
+
+@given(nlp_st.tokens)
+def test_tokens_cache_storage(tokens: Tokens):
+    doc = nlp._spacy_doc_from_tokens(tokens)
+
+    # store the cache
+    nlp.save_caches(Path("/tmp"), file_prefix="pytest")
+    # delete the tokens from the cache
+    del nlp.tokens_to_doc_cache[tokens]
+    # load the cache
+    nlp.load_caches(Path("/tmp"), file_prefix="pytest")
+
+    assert tokens in nlp.tokens_to_doc_cache
+    for token_doc, token_cache in zip(doc, nlp.tokens_to_doc_cache[tokens]):
+        assert str(token_doc) == str(token_cache)
