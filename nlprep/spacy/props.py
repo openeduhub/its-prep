a41c22cbc339f1412d8e79fe2532170c822e56e9
joopitz <jonas.opitz@live.de>
2023-11-06 12:03:49 +0100

    feat: add capacity to persistently store intermediary spacy results
    This can drastically improve the speed of analysis when using the spacy
    backend, as the text analysis process, which only has to be done once for each
    text, takes by far the longest time.

--- nlprep/spacy/props.py
+++ nlprep/spacy/props.py
@@ -16,28 +16,90 @@ from collections.abc import Collection, Callable
 from typing import TypeVar
 import de_core_news_lg
 from nlprep.types import Document, Property_Function, Split_Function, Tokens
+from nlprep.utils import Spacy_defaultdict
 import spacy.tokens
+from pathlib import Path
 
 # spacy NLP pipelines / models
 nlp = de_core_news_lg.load()
 sent_nlp = nlp.add_pipe("sentencizer")
 
-SPACY_PROCESSED_TEXT: set[str] = set()
+
+text_to_doc_cache: Spacy_defaultdict[str] = Spacy_defaultdict(nlp)
+tokens_to_doc_cache: Spacy_defaultdict[Tokens] = Spacy_defaultdict(
+    lambda x: spacy.tokens.Doc(vocab=nlp.vocab, words=list(x))
+)
 
 
-@lru_cache(maxsize=2**16)
+def save_caches(directory: Path, file_prefix: str = "") -> None:
+    """
+    Save intermediary results into the given directory.
+
+    In combination with load_caches, this can allow the user to skip
+    unnecessary re-evaluation of already analyzed texts.
+    """
+    file_prefix = file_prefix + "_" if file_prefix else ""
+    text_to_doc_cache.save(
+        directory / f"{file_prefix}text_to_doc_cache_keys",
+        directory / f"{file_prefix}text_to_doc_cache_docs",
+    )
+    tokens_to_doc_cache.save(
+        directory / f"{file_prefix}tokens_to_doc_cache_keys",
+        directory / f"{file_prefix}tokens_to_doc_cache_docs",
+    )
+
+
+def load_caches(directory: Path, file_prefix: str = "") -> None:
+    """
+    Load intermediary results from the given directory.
+
+    In combination with save_caches, this can allow the user to skip
+    unnecessary re-evaluation of already analyzed texts.
+    """
+
+    _load_text_cache(directory, file_prefix)
+    _load_tokens_cache(directory, file_prefix)
+
+
+def _load_text_cache(directory: Path, file_prefix: str = "") -> None:
+    file_prefix = file_prefix + "_" if file_prefix else ""
+    keys_path = directory / f"{file_prefix}text_to_doc_cache_keys"
+    docs_path = directory / f"{file_prefix}text_to_doc_cache_docs"
+
+    global text_to_doc_cache
+    text_to_doc_cache = Spacy_defaultdict.from_file(
+        default_factory=nlp,
+        keys_path=keys_path,
+        docs_path=docs_path,
+        vocab=nlp.vocab,
+    )
+
+
+def _load_tokens_cache(directory: Path, file_prefix: str = "") -> None:
+    file_prefix = file_prefix + "_" if file_prefix else ""
+    keys_path = directory / f"{file_prefix}tokens_to_doc_cache_keys"
+    docs_path = directory / f"{file_prefix}tokens_to_doc_cache_docs"
+
+    global tokens_to_doc_cache
+    default_factory = lambda x: spacy.tokens.Doc(vocab=nlp.vocab, words=list(x))
+    tokens_to_doc_cache = Spacy_defaultdict.from_file(
+        default_factory=default_factory,
+        keys_path=keys_path,
+        docs_path=docs_path,
+        vocab=nlp.vocab,
+    )
+
+
 def _spacy_doc_from_text(text: str) -> spacy.tokens.Doc:
-    SPACY_PROCESSED_TEXT.add(text)
-    return nlp(text)
+    return text_to_doc_cache[text]
 
 
-@lru_cache(maxsize=2**16)
 def _spacy_doc_from_tokens(tokens: Tokens) -> spacy.tokens.Doc:
     """
     Helper function to turn tokens into processed spaCy docs,
     without re-tokenizing them
     """
-    return spacy.tokens.Doc(vocab=nlp.vocab, words=list(tokens))
+    return tokens_to_doc_cache[tokens]
 
 
 @lru_cache(maxsize=2**16)
@@ -70,7 +132,7 @@ def _document_into_spacy_doc(doc: Document) -> spacy.tokens.Doc:
     """Transform a document into its analyzed spaCy counterpart."""
 
     # if the document was tokenized by spaCy, it was stored during this step
-    if doc.original_text in SPACY_PROCESSED_TEXT:
+    if doc.original_text in text_to_doc_cache:
         return _spacy_doc_from_text(doc.original_text)
 
     # otherwise, return an analyzed version that was not tokenized again
--- nlprep/utils.py
+++ nlprep/utils.py
@@ -1,6 +1,11 @@
+from __future__ import annotations
 from collections import defaultdict
-from collections.abc import Callable
-from typing import Optional, TypeVar
+from collections.abc import Callable, Iterable
+from typing import Optional, TypeVar, Generic
+from pathlib import Path
+import pickle
+import spacy
+import spacy.tokens
 
 T = TypeVar("T")
 _KT = TypeVar("_KT")
@@ -17,3 +22,81 @@ def nest(fun: Callable[[T], T], n: int) -> Callable[[T], T]:
         return x
 
     return nested_fun
+
+
+class Keyed_defaultdict(defaultdict, Generic[_KT, _VT]):
+    """A custom version defaultdict that supports keyed factories"""
+
+    default_factory: Callable[[_KT], _VT]
+
+    def __init__(self, default_factory: Callable[[_KT], _VT]):
+        self.default_factory = default_factory
+
+    def __missing__(self, __key: _KT) -> _VT:
+        """Override the missing method in order to pass the looked up key to the factory"""
+        value = self.default_factory(__key)
+        self[__key] = value
+        return value
+
+    @classmethod
+    def from_file(
+        cls, default_factory: Callable[[_KT], _VT], path: Path
+    ) -> Keyed_defaultdict:
+        """A new keyed defaultdict with data from the given file path."""
+        with open(path, "rb+") as f:
+            # only load the underlying data
+            data = pickle.load(f)
+
+        obj = cls(default_factory)
+        obj.update(data)
+        return obj
+
+    def save(self, path: Path) -> None:
+        """
+        Save the underlying data to the given file path.
+
+        Note that the given default factory is NOT saved -- this is because
+        anonymous or local functions cannot be exported / imported here.
+        """
+        with open(path, "wb+") as f:
+            # only dump the underlying data
+            pickle.dump(dict(self), f)
+
+
+class Spacy_defaultdict(Keyed_defaultdict[_KT, spacy.tokens.Doc]):
+    @classmethod
+    def from_file(
+        cls,
+        default_factory: Callable[[_KT], spacy.tokens.Doc],
+        keys_path: Path,
+        docs_path: Path,
+        vocab: spacy.vocab.Vocab,
+    ) -> Spacy_defaultdict:
+        # load the underlying keys
+        with open(keys_path, "rb") as f:
+            keys: Iterable[_KT] = pickle.load(f)
+
+        # load the underlying documents
+        docbin = spacy.tokens.DocBin()
+        docbin.from_disk(docs_path)
+        docs = docbin.get_docs(vocab)
+
+        # combine them into the new cache
+        obj = cls(default_factory)
+        data = {key: doc for key, doc in zip(keys, docs)}
+        obj.update(data)
+        return obj
+
+    def save(self, keys_path: Path, docs_path: Path) -> None:
+        """
+        Save the underlying keys and documents to the given file paths.
+
+        Note that the given default factory is NOT saved -- this is because
+        anonymous or local functions cannot be exported / imported here.
+        """
+        # dump the keys
+        with open(keys_path, "wb+") as f:
+            pickle.dump(list(self.keys()), f)
+
+        # dump the documents, using the DocBin utility from spacy
+        spacy.tokens.DocBin(docs=self.values()).to_disk(docs_path)
